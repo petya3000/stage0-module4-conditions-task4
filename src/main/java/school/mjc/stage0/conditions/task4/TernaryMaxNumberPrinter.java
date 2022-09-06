0276d7cf45c92b8070bb417daa9a5880d60c766d
petya3000 <99812822+petya3000@users.noreply.github.com>
2022-09-06 11:26:58 +0300

    Update TernaryMaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        first = first > second ? first : second;
+        third = third > first ? third : first;
+        System.out.println(third);
     }
 }
